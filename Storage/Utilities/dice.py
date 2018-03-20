0db93cb021c8fd166308c0faef1d727c5571be16
Shrinath Rao <shrinathrao97@gmail.com>
2018-03-20 12:48:27 -0500

    Dice Roller 0.0

--- Storage/Utilities/dice.py
+++ Storage/Utilities/dice.py
@@ -1,18 +1,38 @@
 import os
 import math
 import sys
+from random import randint
+
+def roll(numDice, numSide):
+	val = ([randint(1, numSide) for i in range(numDice)])
+	print (val)
+	return sum(val)	
 
 #Have easy presets
-print('a. Raw d20 roll')
-int command = input('Enter your command: ')
 
+cont= "y"
+total = 0
+while cont == "y":
+	numDice = input("Enter number of dice: ")
+	numSide = input("Enter number of sides: ")
 
+	numDice = int(numDice)
+	numSide = int(numSide)
 
+	answer = roll(numDice, numSide)
+	print("Your roll: ", answer)
+		
+	total = total + answer
+	print("Your total: ", total)
+	
 
-
-def critSneak():
-hello
-
+	keepGoing = input("Hit enter to continue, hit c to clear total and continue, hit q to quit: ")
+	
+	if keepGoing == "q":
+		cont = "q"	
+	
+	if keepGoing == "c":
+		total = 0
 
 
 
