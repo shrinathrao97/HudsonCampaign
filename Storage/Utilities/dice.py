5a190f29b8ff2453ae578323b31d33260e2615ab
Shrinath Rao <shrinathrao97@gmail.com>
2018-03-21 14:52:10 -0500

    Dice Roller update

--- Storage/Utilities/dice.py
+++ Storage/Utilities/dice.py
@@ -5,6 +5,7 @@ from random import randint
 
 #Define Roll function, generates a list of values rolled (5d6 will generate 5 rolls in a list), returns a sum of the list
 def roll(numDice, numSide):
+
 	val = ([randint(1, numSide) for i in range(numDice)])
 	print (val)
 	return sum(val)	
@@ -18,10 +19,23 @@ total = 0
 
 #While the answer is continue keep repeating
 while cont == "y":
+
+	#Gets input to determine if the while loop should continue
+	print("Enter c to clear total, q to quit, s to skillcheck, or nothing to continue")
+	keepGoing = input("Enter Command: ")
+
+	#Exit condition	
+	if keepGoing == "q":
+		cont = "q"	
+
+	#Clear the running total and continue	
+	if keepGoing == "c":
+		total = 0
+
 	#Gets xdx values
 	numDice = input("Enter number of dice: ")
 	numSide = input("Enter number of sides: ")
-
+	
 	#Converts from string to int
 	numDice = int(numDice)
 	numSide = int(numSide)
@@ -34,16 +48,5 @@ while cont == "y":
 	total = total + answer
 	print("Your total: ", total)
 	
-	#Gets input to determine if the while loop should continue
-	keepGoing = input("Hit enter to continue, hit c to clear total and continue, hit q to quit: ")
-
-	#Exit condition	
-	if keepGoing == "q":
-		cont = "q"	
-
-	#Clear the running total and continue	
-	if keepGoing == "c":
-		total = 0
-
 
 
